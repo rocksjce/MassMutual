9bd19bf3f460cfa7a496247469e9a720b8e06f61
SHARMA <rohit.sharma.ext@siemensgamesa.com>
2021-01-13 14:19:30 +0530

    addional changes

--- features/MassMutual/pages/calculation_check.rb
+++ features/MassMutual/pages/calculation_check.rb
@@ -23,93 +23,56 @@ class UtilityPage
     @txt_val_ttl = @browser.text_field(:id, 'txt_ttl_val')
   end
 
-  # Purpose - Navigating to Clear Trip site
+  # Purpose - Navigating to site URL
   def visit
     @browser.goto(URL)
   end
 
+  # Purpose - get total count of the value field of amount balance
   def check_for_val_count
     @browser.elements(:class => "txt_val_.*").size
   end
 
-  def search_flight
-    @trip_type.wait_until(timeout: 60, &:present?).click! unless @modal_popup.exists?
-    @source_name.wait_until(timeout: 60, &:present?).set SOURCE_PLACE_NAME
-    @destination_name.wait_until(timeout: 60, &:present?).set DESTINATION_PLACE_NAME
-    @depart_date.wait_until(timeout: 60, &:present?).set DEPART_DATE
-    @return_date.wait_until(timeout: 60, &:present?).set RETURN_DATE
-    @no_of_adult.wait_until(timeout: 60, &:present?).select ADULT_COUNT.to_s
-    @no_of_child.wait_until(timeout: 60, &:present?).select CHILD_COUNT.to_s
-    @search_btn.wait_until(timeout: 60, &:present?).click!
-    wait_until_true(150){@last_flight[2].lis[4].label.visible?}
-    # Choosing the last flight on very next day
-    @last_flight[2].lis[4].label.wait_until(timeout: 60, &:present?).click!
-    p 'Click on the book button'
-    @browser.buttons.each do |btn|
-      btn.click! if btn.text == 'Book'
-    end
-  end
-
-  # Purpose - Booking the flight ticket
-  # Params - Passenger details - Hash value detail of person like name, dob, nationality
-  def booking_of_flight(passenger_details)
-    @modal_popup.a.click! if @modal_popup.exists? # if pop-up relating to "Try Again" comes, then close it
-    @itinerary_btn.wait_until(timeout: 120, &:present?).click!
-    sleep(10)
-
-    @adult_iterator = 1
-    @child_iterator = 1
-    @iterator = 1
-    passenger_details.each do |passgr|
-      person_type = passgr['Person']
-      title = passgr['Passenger_name'].split(' ')[0]
-      first_name = passgr['Passenger_name'].split(' ')[1]
-      second_name = passgr['Passenger_name'].split(' ')[2]
-      dob_day = passgr['DateOfBirth'].split('/')[0]
-      dob_month = passgr['DateOfBirth'].split('/')[1]
-      dob_year = passgr['DateOfBirth'].split('/')[2]
-      nationality = passgr['Nationality']
-      val = person_type == 'Adult' ? @adult_iterator : @child_iterator
-      @browser.select_list(:id, "#{person_type}Title#{val}").select title
-      @browser.text_field(:id, "#{person_type}Fname#{val}").set first_name
-      @browser.text_field(:id, "#{person_type}Lname#{val}").set second_name
-      @browser.select_list(:id, "#{person_type}DobDay#{val}").select dob_day if @browser.select_list(:id, "#{person_type}DobDay#{val}").exists?
-      @browser.select_list(:id, "#{person_type}DobMonth#{val}").select dob_month if @browser.select_list(:id, "#{person_type}DobMonth#{val}").exists?
-      @browser.select_list(:id, "#{person_type}DobYear#{val}").select dob_year if @browser.select_list(:id, "#{person_type}DobYear#{val}").exists?
-      @nationality[@iterator - 1].set nationality if @nationality.count > 0
-      @adult_iterator += 1 if person_type == 'Adult'
-      @child_iterator += 1 if person_type == 'Child'
-      @iterator += 1
-    end
-    p 'Provide your contact details' if @mobile_number.set MOBILE_NUMBER
-    @travel_continue_btn.wait_until(timeout: 60, &:present?).click!
-    sleep(10)
-    @modal_popup.a.click! if @modal_popup.exists?
-  end
-
-  # Purpose - Make payment for the flight ticket
-  # Params - nil
-  def flight_payment_gateway
+  # Purpose - Verify whether values mentioned on the screen are greater than 0 (val: provided as input)
+  def verify_val(val, tot_val_count)
     res = false
-    begin
-      res = true if @payment_submit_btn.wait_until(timeout: 60, &:present?).exists?
-      p 'Payment for the flight ticket follows from HERE'
-    rescue
-      res = false
+    case tot_val_count
+    when 1
+      res = @txt_val1.text.gsub(/[$]/, '$' => '').to_f > val
+    when 2
+      res = @txt_val2.text.gsub(/[$]/, '$' => '').to_f > val
+    when 3
+      res = @txt_val3.text.gsub(/[$]/, '$' => '').to_f > val
+    when 4
+      res = @txt_val4.text.gsub(/[$]/, '$' => '').to_f > val
+    when 5
+      res = @txt_val5.text.gsub(/[$]/, '$' => '').to_f > val
     end
     res
   end
 
-  # Purpose - Method to make block wait until block returns TRUE
-  def wait_until_true(seconds = 150)
-    end_time = Time.now + seconds
-    begin
-      yield
-    rescue StandardError => e
-      raise e.message if Time.now >= end_time
-      puts "Got #{e.message}, hence retrying...!"
-      sleep 3
-      retry
-    end
+  # Purpose - check whether total balance matches with the sum of values mentioned on the screen
+  def check_for_tot_bal
+    val1 = @txt_val1.text.gsub(/[$]/, '$' => '')
+    val2 = @txt_val2.text.gsub(/[$]/, '$' => '')
+    val3 = @txt_val3.text.gsub(/[$]/, '$' => '')
+    val4 = @txt_val4.text.gsub(/[$]/, '$' => '')
+    val5 = @txt_val5.text.gsub(/[$]/, '$' => '')
+    total_val_expected = [val1, val2, val3, val4, val5].map(&:to_f).inject(:+)
+    total_val_actual = @txt_val_ttltext.gsub(/[$]/, '$' => '').to_f
+    total_val_expected == total_val_actual
   end
+
+  # Purpose - Verify for the currency of values
+  def check_for_curr
+    res = false
+    not_matched_currency = []
+    val_arr = [@txt_val1, @txt_val2, @txt_val3, @txt_val4, @txt_val5, @txt_val_ttl]
+    val_arr.each do |val|
+      not_matched_currency = false unless val.text.to_s.chr == "$"
+    end
+    res = false unless not_matched_currency.empty?
+    res
+  end
+
 end
--- features/MassMutual/step_definitions/calculation_steps.rb
+++ features/MassMutual/step_definitions/calculation_steps.rb
@@ -10,23 +10,36 @@ When(/^calculation check page available$/) do
 end
 
 Then(/^value count on the screen should be (.*)$/) do |val|
-  total_val_count = @mass_mutual.check_for_val_count
-  raise "value count on the screen #{total_val_count} didn't match with #{val}" unless total_val_count == val
-  logger.info "value count on the screen #{total_val_count} validated" if total_val_count == val
+  @total_val_count = @mass_mutual.check_for_val_count
+  raise "value count on the screen #{@total_val_count} didn't match with #{val}" unless @total_val_count == val
+  logger.info "value count on the screen #{@total_val_count} validated" if @total_val_count == val
 end
 
-Then(/^value on the screen should be greater than (\d+)$/) do |count|
-  @mass_mutual.booking_of_flight(count)
+Then(/^value on the screen should be greater than (\d+)$/) do |val|
+  tot_val_count = @txt_val_count
+  failed_arr = []
+  while tot_val_count >= 1
+    failed_arr << tot_val_count unless @mass_mutual.verify_val(val, tot_val_count)
+    tot_val_count = tot_val_count - 1
+  end
+  raise "value on the screen are not greater than #{val} for #{failed_arr}" unless failed_arr.empty?
+  logger.info "All values mentioned on screen are greater than #{val}"
 end
 
 Then(/^total balance should be correct based on the values listed on the screen$/) do
-  pending
+  tot_bal_res = @mass_mutual.check_for_tot_bal
+  raise "Total balance are NOT correct based on the values listed on the screen " unless tot_bal_res
+  logger.info "Total balance are correct based on the values listed on the screen"
 end
 
 Then(/^values are formatted as currencies$/) do
-  pending
+  res = @mass_mutual.check_for_curr
+  raise "Values are NOT formatted as currencies listed on the screen" unless res
+  logger.info "Values are formatted as currencies as listed on the screen"
 end
 
 Then(/^the total balance matches the sum of the values$/) do
-  pending
+  res = @mass_mutual.check_for_tot_bal
+  raise "Total balance doesn't matches the sum of the values mentioned on the screen" unless res
+  logger.info "Total balance matches the sum of the values mentioned on the screen"
 end
